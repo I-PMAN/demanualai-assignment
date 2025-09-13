9f8ec295a77be4b437266a9613fb58d9a0de35e7
Parth <ippman@localhost.localdomain>
2025-09-13 11:31:19 +0530

    Cleaning main.py

--- api/main.py
+++ api/main.py
@@ -17,32 +17,36 @@ class PostRequest(BaseModel):
 def generate_post(request: PostRequest):
     gemini_api_key = os.getenv("API_KEY")
     client = genai.Client(api_key=gemini_api_key)
-    result = {}
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="",
-    )
+
     topic = request.topic
     articles = get_articles(topic)
 
-    urls = []
-    for article in articles["organic"]:
-        urls.append(article["link"])
-    result["topic"] = topic
-    result["news_sources"] = urls
-    ai_query = f"Use the top 3 sources and generate a LinkedIn-style post for the topic {topic} summarizing the news in {result}. The tone of the post should be professional and engaging. Do not add any extra text in the response."
+    urls = [a.get("link") for a in articles.get("organic", [])[:3]]
+
+    ai_query = (
+            f"Using the following sources {urls}, write a LinkedIn-style post"
+            f"summarizing the latest news about '{topic}'."
+            "Keep it professional and engaging."
+            "End with a thought-provoking note."
+            "Do not include extra text outside the post."
+    )
+
     response = client.models.generate_content(
             model="gemini-2.5-flash",
-    contents=ai_query)
-    
-    linkedin_post = response.candidates[0].content.parts[0].text
-    result["linkedin_post"] = linkedin_post
-    return{"response" : result}
+            contents=[ai_query]
+    )
 
+    linkedin_post = response.candidates[0].content.parts[0].text
+
+    return {
+            "toppic" : topic,
+            "news_sources" : urls,
+            "linkedin_post" : linkedin_post,
+            "image_suggestion" : None
+            }
 
 def get_articles(topic):
     os.environ["SERPER_API_KEY"] = os.getenv("SERPER_KEY")
     search = GoogleSerperAPIWrapper()
-    query = f"Give me a list of 3 latest news or articles with sources for the topic {topic}. Also include an image suggestion if possible."
-    result = search.results(query)
-    return result
+    query = f"latest 3 news articles about {topic}"
+    return search.results(query)
